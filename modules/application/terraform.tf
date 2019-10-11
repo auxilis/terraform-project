4c06d6768ea570c208fc5c5636ec3e6d22cd0e2e
auxilis <56384852+auxilis@users.noreply.github.com>
2019-10-11 11:58:28 +0200

    Update terraform.tf

--- modules/application/terraform.tf
+++ modules/application/terraform.tf
@@ -2,7 +2,7 @@ terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
     organization = "test_org_wk"
-    token        = "e7djgqdlQ2WXjQ.atlasv1.q6qdNaCThwjIRYoTkWlR6VVHDaZjs7W0PoN2NDtb3QDe5ZJauWBsAX8iD1R4Ee5izaI"
+    token        = ""
 
     workspaces {
       prefix = "prj-app-"
